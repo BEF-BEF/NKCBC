114317519fb9280033217214fd144d1fa9067914
ryanperrycarlson <ryanperrycarlson@gmail.com>
2018-10-26 23:53:11 -0500

    Add files via upload

--- reactApp/actualApp/navigation/AppNavigator.js
+++ reactApp/actualApp/navigation/AppNavigator.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 import homeScreen from '../screens/HomeScreen';
-import logInScreen from '../screens/logInScreen'
+//import logInScreen from '../screens/logInScreen'
+import logInScreen from '../screens/logInScreen/user_regv2'
 /*
 public function changeScreen(var pageToChangeTo) {
   return
 }
 */
-export default createSwitchNavigator({
+export default createStackNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
   HomeScreen: homeScreen,
   LogIn:logInScreen,
+
 
 
 });
